0cfccbaee48c3185dbb04aecdd71a1d50cc87f04
DeFiMonk <70180525+DeFiMonk@users.noreply.github.com>
2021-11-12 19:15:44 +0800

    Audit Fix

--- contracts/Staking.sol
+++ contracts/Staking.sol
@@ -4,13 +4,10 @@ pragma solidity 0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract Staking is Ownable, ReentrancyGuard {
     IERC20 public stakingToken;
-    
-    using SafeMath for uint256;
     
     uint256 public noOfStakes;
     uint256 public totalStake;
